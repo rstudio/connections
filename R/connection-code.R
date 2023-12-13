4cf043ef34e26951785af15f5c8d29858b5ca6d3
Edgar Ruiz <edgararuiz@gmail.com>
2023-12-13 15:09:52 -0600

    Documents connection_code()

--- R/connection-code.R
+++ R/connection-code.R
@@ -1,3 +1,5 @@
+#' Displays the code that will be used to recreate the connection
+#' @param con A `connConnection` object
 #' @export
 connection_code <- function(con) {
   session <- conn_session_get(con@id)
